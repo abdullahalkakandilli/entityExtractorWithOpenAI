bf90a72d2bebcfc9815912dad1bc0c38590e5a30
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-03-02 11:01:46 +0100

    df update

--- main.py
+++ main.py
@@ -70,7 +70,8 @@ def entity_extractor():
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system",
-             "content": "Answer following questions for the given article: What are the names,locations, ids, phone numbers, company names and emails. And then create a two column dataframe with 'Entity name' and 'Entity value'. Put responses to the related column as a new row. Only respond dataframe. Nothing else. Only check these values. Don't check other values or entities. There might be more than one name, location, id, phone number,company, email or iban. If you find more tha one, add new row with entity name and value."
+             "content": "Find requested entities from the given article. Requested entities are 'name,location(it might be a hospital, office name etc.),id,data,company,email,phone number, iban. Don't create or try to find entities other than requested entities. Create a dataframe with two column which are the names are 'Entity name' and 'Entity value' and put the values accordingly."
+                        "There might be more than one name, location, id, phone number,company, email or iban. If you find more tha one, add new row with entity name and value."
                         "Given article: " +"'" +  pdf_text_result_ + "'"}
 
         ]
